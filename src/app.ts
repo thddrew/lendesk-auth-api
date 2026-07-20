6ec64dd1b245f7306967cd71a433856d8d17e728
thddrew <thddrew@gmail.com>
2026-07-19 22:23:33 -0700

    doc: update comment explaining chained methods on hono instance

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,6 @@ import { logger } from "hono/logger";
 import { errorLogger } from "./utils/errors/logger.js";
 import { redisClient } from "./db/redis.js";
 
-// Chained
 const initApp = new Hono()
   .use(logger())
   // Default CORS config, but should customize for your deployment environment
@@ -14,6 +13,7 @@ const initApp = new Hono()
   .notFound((c) => c.json({ error: "Resource Not Found" }, 404))
   .onError(errorLogger);
 
+// Chained to support a typed test client https://arc.net/l/quote/wafwvhky
 const routes = initApp
   .get("/healthcheck", async (c) => {
     const db = await redisClient.ping();
